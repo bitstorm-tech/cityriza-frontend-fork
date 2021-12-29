7e7bc221835943f98f96464736a46ab37605796c
Josef Bauer <josef.bauer.1st@gmail.com>
2021-12-30 00:41:01 +0100

    Enable http service to work with form data

--- src/lib/http.service.ts
+++ src/lib/http.service.ts
@@ -1,12 +1,12 @@
 const BACKEND_HOST = import.meta.env.VITE_BACKEND_HOST;
-const headers = { 'Content-Type': 'application/json' };
 
 export function createUrl(path: string): string {
   return `${BACKEND_HOST}${path}`;
 }
 
-export function post(path: string, payload): Promise<Response> {
+export function post(path: string, payload, isFormData = false): Promise<Response> {
   const url = createUrl(path);
-  const body = JSON.stringify(payload);
+  const body = isFormData ? payload : JSON.stringify(payload);
+  const headers = isFormData ? {} : { 'Content-Type': 'application/json' };
   return fetch(url, { method: 'POST', body, headers });
 }

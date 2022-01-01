3d5459995cf8884fdbeff82a5100d0de2e312c25
Josef Bauer <josef.bauer.1st@gmail.com>
2022-01-01 01:14:43 +0100

    Call sign-in endpoint

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -2,15 +2,32 @@
   import Link from '../components/ui/Link.svelte';
   import Input from '../components/ui/Input.svelte';
   import Button from '../components/ui/Button.svelte';
+  import { post } from '$lib/http.service';
+  import { goto } from '$app/navigation';
+  import WarningMessage from '../components/ui/WarningMessage.svelte';
+
+  let email: string;
+  let password: string;
+  let error = false;
+
+  async function signIn() {
+    const response = await post('/api/auth/signin', { email, password });
+    if (response.ok) {
+      goto('/');
+    } else {
+      error = true;
+    }
+  }
 </script>
 
 <div class="flex items-center justify-center mt-10 mb-32" style="padding-top: 3rem;">
   <div class="w-5/6 lg:w-1/3 space-y-3">
     <h1 class="text-center font-caveat text-4xl mb-4">Bei Cityriza anmelden</h1>
-    <Input label="E-Mail" type="email" />
-    <Input label="Passwort" type="password" />
+    <WarningMessage show={error}>Falsche E-Mail oder falsches Passwort</WarningMessage>
+    <Input label="E-Mail" type="email" bind:value={email} />
+    <Input label="Passwort" type="password" bind:value={password} />
     <div class="flex flex-col space-y-2">
-      <Button>Anmelden</Button>
+      <Button on:click={signIn}>Anmelden</Button>
       <div class="flex text-xs space-x-1">
         <span>Noch keinen Account?</span>
         <Link href="/registration">Registrieren</Link>
